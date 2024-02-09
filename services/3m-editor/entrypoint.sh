a08bad5dd73fe440df9f23e1bf4c19a688941142
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-02-09 13:39:23 +0100

    add [prts

--- services/3m-editor/entrypoint.sh
+++ services/3m-editor/entrypoint.sh
@@ -10,25 +10,33 @@ fi
 # inject ip address for 3m
 echo "injecting the IP adress $IP_ADDRESS"
 IP_ADDRESS=0.0.0.0
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/3M/WEB-INF/web.xml
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/3MEditor/WEB-INF/web.xml
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/Maze/index.html
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/Maze/errorpage.html
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/Maze/multmappings.html
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/Maze/singlemapping.html
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/Maze/WEB-INF/config.properties
-sed -i s/255.255.255.255:8080/$IP_ADDRESS/g $CATALINA_HOME/webapps/Maze/app/js/Controller.js
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/3M/WEB-INF/web.xml
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/3MEditor/WEB-INF/web.xml
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/Maze/index.html
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/Maze/errorpage.html
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/Maze/multmappings.html
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/Maze/singlemapping.html
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/Maze/WEB-INF/config.properties
+sed -i s/255.255.255.255:8080/$IP_ADDRESS:8080/g $CATALINA_HOME/webapps/Maze/app/js/Controller.js
 
 # injecting the db name
 echo "injecting the db name $DB_NAME"
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/3M/WEB-INF/web.xml
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/3MEditor/WEB-INF/web.xml
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/Maze/index.html
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/Maze/errorpage.html
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/Maze/multmappings.html
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/Maze/singlemapping.html
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/Maze/WEB-INF/config.properties
-sed -i s/255.255.255.255:8081/$DB_NAME/g $CATALINA_HOME/webapps/Maze/app/js/Controller.js
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/3M/WEB-INF/web.xml
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/3MEditor/WEB-INF/web.xml
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/index.html
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/errorpage.html
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/multmappings.html
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/singlemapping.html
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/WEB-INF/config.properties
+sed -i s/255.255.255.255:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/app/js/Controller.js
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/3M/WEB-INF/web.xml
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/3MEditor/WEB-INF/web.xml
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/index.html
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/errorpage.html
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/multmappings.html
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/singlemapping.html
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/WEB-INF/config.properties
+sed -i s/0.0.0.0:8081/$DB_NAME:8081/g $CATALINA_HOME/webapps/Maze/app/js/Controller.js
 
 # start eXist-db and Apache Tomcat
 exec supervisord -n
