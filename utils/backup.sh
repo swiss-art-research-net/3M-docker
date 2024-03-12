c0ffcc027a1340d11bcff0cde01d57a026f503ff
Florian Kräutli <florian.kraeutli@uzh.ch>
2024-03-12 15:35:57 +0100

    delete older backups

--- utils/backup.sh
+++ utils/backup.sh
@@ -17,11 +17,32 @@ BIND_BACKUP=${BACKUP_BASE_PATH}/binds_${BACKUP_DATE}.tar.gz
 tar -C ${BACKUP_PATH} -czf ${BIND_BACKUP} .
 
 echo "[I] Uploading backup"
-aws s3 cp "${BIND_BACKUP}" "s3://${AWS_BUCKET}" --endpoint-url "${AWS_ENDPOINT}"
+#aws s3 cp "${BIND_BACKUP}" "s3://${AWS_BUCKET}" --endpoint-url "${AWS_ENDPOINT}"
 
 rm "${BIND_BACKUP}"
 
 echo "[I] Finished backup at $(date +%Y-%m-%d_%H-%M)"
 for i in $(seq 1 3); do printf "\n"; done
 
+
+# Delete backups older than 100 days
+BACKUP_PREFIX="binds_"
+BACKUP_DATE_FORMAT="+%Y-%m-%d_%H-%M"
+
+# Get the current date
+CURRENT_DATE=$(date ${BACKUP_DATE_FORMAT})
+
+# Calculate the date threshold
+BACKUP_THRESHOLD_DATE=$(date -v -${BACKUP_DAYS_THRESHOLD}d ${BACKUP_DATE_FORMAT})
+
+# List all objects in the S3 bucket
+BACKUP_OBJECTS=$(aws s3api list-objects --bucket "${AWS_BUCKET}" --endpoint-url "${AWS_ENDPOINT}" --prefix "${BACKUP_PREFIX}" --query 'Contents[?LastModified<`'"${BACKUP_THRESHOLD_DATE}"'`].Key' --output text)
+
+# Delete the backup objects
+if [[ -n "${BACKUP_OBJECTS}" ]]; then
+    echo "[I] Deleting backups older than ${BACKUP_DAYS_THRESHOLD} days"
+    aws s3api delete-objects --bucket "${AWS_BUCKET}" --endpoint-url "${AWS_ENDPOINT}" --delete "$(jq -n --arg objects "${BACKUP_OBJECTS}" '{Objects: ($objects | split("\n")[:-1] | map({Key: .}))}')"
+else
+    echo "[I] No backups found older than ${BACKUP_DAYS_THRESHOLD} days"
+fi
 exit 0
